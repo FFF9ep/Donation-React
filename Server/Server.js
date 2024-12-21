ae6d62a0e808784df43da7454699ca5b5160d288
fandi a <fandiiardy@gmail.com>
2024-12-21 09:15:34 +0700

    first commit

---
+++ Server/Server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
